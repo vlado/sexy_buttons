e3ecd990a6f80c3c5c8dc2cea896d8d0863900c7
Vlado Cingel <vladocingel@gmail.com>
2009-03-18 18:24:40 +0100

    readme and install hook updated

--- install.rb
+++ install.rb
@@ -1,6 +1,10 @@
 puts "\n"
 puts "* Installing 'SexyButtons' plugin"
-FileUtils.cp_r("#{RAILS_ROOT}/vendor/plugins/sexy_buttons/public/stylesheets/sexy_buttons", "#{RAILS_ROOT}/public/stylesheets/sexy_buttons")
-puts "+ Stylesheets and themes copied to /public/stylesheets/sexy_buttons"
+if File.exist?("#{RAILS_ROOT}/public/stylesheets/sexy_buttons")
+  puts "! Directory '#{RAILS_ROOT}/public/stylesheets/sexy_buttons' already exists. You can manualy copy (update) resources from '#{RAILS_ROOT}/vendor/plugins/sexy_buttons/public/stylesheets/sexy_buttons'."
+else
+  FileUtils.cp_r("#{RAILS_ROOT}/vendor/plugins/sexy_buttons/public/stylesheets/sexy_buttons", "#{RAILS_ROOT}/public/stylesheets/sexy_buttons")
+  puts "+ Stylesheets and themes copied to /public/stylesheets/sexy_buttons"
+end
 puts "+ Plugin installed. Visit http://github.com/vlado/sexy_buttons for more info."
 puts "\n"

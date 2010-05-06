7bd3aa29b8a22cd5792c373ce24586637789bc22
Vlado Cingel <vladocingel@gmail.com>
2010-05-06 10:46:33 +0200

    generator to copy assets added, install hook updated to use assets generator

--- generators/sexy_buttons/sexy_buttons_generator.rb
+++ generators/sexy_buttons/sexy_buttons_generator.rb
@@ -3,10 +3,23 @@ class SexyButtonsGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
       case name
+      when "assets"
+        themes = Dir.entries("#{RAILS_ROOT}/vendor/plugins/sexy_buttons/generators/sexy_buttons/templates/assets/themes").delete_if { |f| f.match(/^\./) }
+        theme_files = ["style.css", "bg.png"]
+        m.directory "public/stylesheets/sexy_buttons"
+        m.file "assets/style.css", "public/stylesheets/sexy_buttons/style.css"
+        m.directory "public/stylesheets/sexy_buttons/themes"
+        themes.each do |theme|
+          m.directory "public/stylesheets/sexy_buttons/themes/#{theme}"
+          theme_files.each do |file_name|
+            m.file "assets/themes/#{theme}/#{file_name}", "public/stylesheets/sexy_buttons/themes/#{theme}/#{file_name}"
+          end
+        end
+        logger.info "Copying sexy buttons assets to public/stylesheets/sexy_buttons"
       when "theme"
         theme_name = args[0]
         m.directory "public/stylesheets/sexy_buttons/themes/#{theme_name}"
-        m.template "style.css.erb", "public/stylesheets/sexy_buttons/themes/#{theme_name}/style.css", :assigns => { :theme_name => theme_name, :font_size => args[1], :height => args[2] }
+        m.template "theme/style.css.erb", "public/stylesheets/sexy_buttons/themes/#{theme_name}/style.css", :assigns => { :theme_name => theme_name, :font_size => args[1], :height => args[2] }
       end
     end
   end
--- install.rb
+++ install.rb
@@ -1,10 +1,1 @@
-puts "\n"
-puts "* Installing 'SexyButtons' plugin"
-if File.exist?("#{RAILS_ROOT}/public/stylesheets/sexy_buttons")
-  puts "! Directory '#{RAILS_ROOT}/public/stylesheets/sexy_buttons' already exists. You can manualy copy (update) resources from '#{RAILS_ROOT}/vendor/plugins/sexy_buttons/public/stylesheets/sexy_buttons'."
-else
-  FileUtils.cp_r("#{RAILS_ROOT}/vendor/plugins/sexy_buttons/public/stylesheets/sexy_buttons", "#{RAILS_ROOT}/public/stylesheets/sexy_buttons")
-  puts "+ Stylesheets and themes copied to /public/stylesheets/sexy_buttons"
-end
-puts "+ Plugin installed. Visit http://github.com/vlado/sexy_buttons for more info."
-puts "\n"
+system("script/generate sexy_buttons assets")
